781af48197128edbdcccfd5e0dfadb5ea8b2fb70
jonpecar <jonathan.pecar@outlook.com>
2022-09-22 17:56:49 +1000

    Partially writen code for image grid display

--- qrImageIndexerGUI/sort_images_window.py
+++ qrImageIndexerGUI/sort_images_window.py
@@ -1,10 +1,38 @@
 from textwrap import wrap
 import tkinter as tk
 from PIL import ImageTk, Image, ImageOps
+from typing import Dict
 
 class ImageGrid(tk.Frame):
-    def __init__(self, master, *args, **kwargs):
+    def __init__(self, master, column_wdith :int = 400, *args, **kwargs):
         tk.Frame.__init__(self, master, *args, **kwargs)
+        self.images : Dict[str, KeyImagePresent] = {}
+        self.column_width = 400
+
+        self.vscrollbar = tk.Scrollbar(self)
+        self.vscrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.scrolled_canvas = tk.Canvas(self, bd=0, highlightthickness=0,
+                                        yscrollcommand=self.vscrollbar.set)
+        self.scrolled_canvas.pack(side=tk.LEFT, fill=tk.BOTH)
+        self.vscrollbar.config(command=self.scrolled_canvas.yview)
+
+        self.scrolled_canvas.xview_moveto(0)
+        self.scrolled_canvas.yview_moveto(0)
+
+        self.scrolled_canvas.bind('<Configure>', self.resize)
+
+    def resize(self, event):
+        size = (event.width, event.height)
+        columns = self.column_width//size[0]
+        
+        if columns == 0:
+            columns = 1
+        
+        
+        
+
+
 
 
 class KeyImagePresent(tk.Frame):
@@ -12,23 +40,30 @@ class KeyImagePresent(tk.Frame):
     def __init__(self, image_path : str, extracted_path : str, master, *args, **kwargs):
         tk.Frame.__init__(self, master, *args, **kwargs)
         self.columnconfigure(0, weight=1)
-        self.rowconfigure(0, weight=1)
-        self.rowconfigure(1, weight=0)
+        self.rowconfigure(0, weight=0)
+        self.rowconfigure(1, weight=1)
+        self.rowconfigure(2, weight=0)
         
         self.original_image = Image.open(image_path)
         self.image = ImageTk.PhotoImage(self.original_image)
         self.image_canv = tk.Canvas(self, bd=0, highlightthickness=0)
         self.image_canv.create_image(0, 0, image=self.image, anchor=tk.NW, tags='IMG')
         # self.image_canv.pack(side=tk.TOP, fill=tk.BOTH)
-        self.image_canv.grid(row=0, sticky=tk.NSEW)
+        self.image_canv.grid(row=1, sticky=tk.NSEW)
 
         self.bind('<Configure>', self.resize)
         # self.image_lbl = tk.Label(self, image=self.image)
         # self.image_lbl.pack(side=tk.TOP, fill=tk.BOTH)
 
-        self.path_lbl = tk.Label(self, text=extracted_path, justify=tk.CENTER)
+        self.path = extracted_path
+        self.path_lbl = tk.Label(self, text=self.path, justify=tk.CENTER)
         # self.path_lbl.pack(side=tk.BOTTOM, fill=tk.BOTH)
-        self.path_lbl.grid(row=1, sticky=tk.NSEW)
+        self.path_lbl.grid(row=2, sticky=tk.NSEW)
+
+        self.image_path = image_path
+        self.image_path_lbl = tk.Label(self, text=self.image_path, justify=tk.CENTER)
+        # self.path_lbl.pack(side=tk.BOTTOM, fill=tk.BOTH)
+        self.image_path_lbl.grid(row=0, sticky=tk.NSEW)
 
     def resize(self, event):
         size = (event.width, event.height)
